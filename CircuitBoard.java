665583f49657d9ef92fa5987ef0f6993b3aa58da
Davina <davinacausey@u.boisestate.edu>
2024-12-03 14:34:16 -0700

    initial

---
+++ CircuitBoard.java
@@ -0,0 +1,211 @@
+import java.awt.Point;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Represents a 2D circuit board as read from an input file.
+ *  
+ * @author mvail
+ */
+public class CircuitBoard {
+	/** current contents of the board */
+	private char[][] board;
+	/** location of row,col for '1' */
+	private Point startingPoint;
+	/** location of row,col for '2' */
+	private Point endingPoint;
+
+	//constants you may find useful
+	private final int ROWS; //initialized in constructor
+	private final int COLS; //initialized in constructor
+	private final char OPEN = 'O';	//capital 'o', an open position
+	private final char CLOSED = 'X';//a blocked position
+	private final char TRACE = 'T';	//part of the trace connecting 1 to 2
+	private final char START = '1';	//the starting component
+	private final char END = '2';	//the ending component
+	private final String ALLOWED_CHARS = "OXT12"; //useful for validating with indexOf
+
+	/** Construct a CircuitBoard from a given board input file, where the first
+	 * line contains the number of rows and columns as ints and each subsequent
+	 * line is one row of characters representing the contents of that position.
+	 * Valid characters are as follows:
+	 *  'O' an open position
+	 *  'X' an occupied, unavailable position
+	 *  '1' first of two components needing to be connected
+	 *  '2' second of two components needing to be connected
+	 *  'T' is not expected in input files - represents part of the trace
+	 *   connecting components 1 and 2 in the solution
+	 * 
+	 * @param filename
+	 * 		file containing a grid of characters
+	 * @throws FileNotFoundException if Scanner cannot open or read the file
+	 * @throws InvalidFileFormatException for any file formatting or content issue
+	 */
+	public CircuitBoard(String filename) throws FileNotFoundException {
+		Scanner file = new Scanner(new File(filename));
+		try{
+			String fileLine = file.nextLine();
+			Scanner line = new Scanner(fileLine);
+			if(line.hasNextInt()){
+				ROWS = line.nextInt();
+			}else{
+				file.close();
+				line.close();
+				throw new InvalidFileFormatException("First line of file must contain two integers");
+			}
+			if(line.hasNextInt()){
+				COLS = line.nextInt();
+			}else{
+				file.close();
+				line.close();
+				throw new InvalidFileFormatException("First line of file must contain two integers");
+			}
+			board = new char[ROWS][COLS];
+			int rowCount = 0;
+			int colCount = 0;
+			int startCount = 0;
+			int endCount = 0;
+			
+			//parse the given file to populate the char[][]
+			while(file.hasNextLine()){
+				while(line.hasNext()){
+					for (int i = 0; i < ROWS; i++) {
+						for (int j = 0; j < COLS; j++) {
+							if(board[i][j] == START){
+								startCount++;
+							}
+							if(board[i][j] == END){
+								endCount++;
+							}
+							board[i][j] = line.next().charAt(j);
+							colCount++;
+						}
+						rowCount++;
+					}
+					if(colCount != COLS){
+						file.close();
+						line.close();
+						throw new InvalidFileFormatException("Incorrect number of columns");
+					}
+					if(rowCount != ROWS){
+						file.close();
+						line.close();
+						throw new InvalidFileFormatException("Incorrect number of rows");
+					}
+					if(startCount != 1 || endCount != 1){
+						file.close();
+						line.close();
+						throw new InvalidFileFormatException("No starting or end point found");
+					}
+
+					
+				
+				}
+			}
+		
+		
+			
+		}
+		catch(FileNotFoundException e){
+			throw new FileNotFoundException("Scanner cannot open or read the file");
+		}
+	
+
+		
+		file.close();
+	}
+	
+	/** Copy constructor - duplicates original board
+	 * 
+	 * @param original board to copy
+	 */
+	public CircuitBoard(CircuitBoard original) {
+		board = original.getBoard();
+		startingPoint = new Point(original.startingPoint);
+		endingPoint = new Point(original.endingPoint);
+		ROWS = original.numRows();
+		COLS = original.numCols();
+	}
+
+	/** Utility method for copy constructor
+	 * @return copy of board array */
+	private char[][] getBoard() {
+		char[][] copy = new char[board.length][board[0].length];
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				copy[row][col] = board[row][col];
+			}
+		}
+		return copy;
+	}
+	
+	/** Return the char at board position x,y
+	 * @param row row coordinate
+	 * @param col col coordinate
+	 * @return char at row, col
+	 */
+	public char charAt(int row, int col) {
+		return board[row][col];
+	}
+	
+	/** Return whether given board position is open
+	 * @param row
+	 * @param col
+	 * @return true if position at (row, col) is open 
+	 */
+	public boolean isOpen(int row, int col) {
+		if (row < 0 || row >= board.length || col < 0 || col >= board[row].length) {
+			return false;
+		}
+		return board[row][col] == OPEN;
+	}
+	
+	/** Set given position to be a 'T'
+	 * @param row
+	 * @param col
+	 * @throws OccupiedPositionException if given position is not open
+	 */
+	public void makeTrace(int row, int col) {
+		if (isOpen(row, col)) {
+			board[row][col] = TRACE;
+		} else {
+			throw new OccupiedPositionException("row " + row + ", col " + col + "contains '" + board[row][col] + "'");
+		}
+	}
+	
+	/** @return starting Point(row,col) */
+	public Point getStartingPoint() {
+		return new Point(startingPoint);
+	}
+	
+	/** @return ending Point(row,col) */
+	public Point getEndingPoint() {
+		return new Point(endingPoint);
+	}
+	
+	/** @return number of rows in this CircuitBoard */
+	public int numRows() {
+		return ROWS;
+	}
+	
+	/** @return number of columns in this CircuitBoard */
+	public int numCols() {
+		return COLS;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				str.append(board[row][col] + " ");
+			}
+			str.append("\n");
+		}
+		return str.toString();
+	}
+	
+}// class CircuitBoard
